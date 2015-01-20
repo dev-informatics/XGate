0fffde99f0b15cd42efe85f60087a3ef5832a445
realityenigma <realityenigma@hotmail.com>
2015-01-20 11:35:18 -0600

    Added EnabledDateRange State.

---
+++ XGate.Core/States/EnabledDateRangeXGateState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGate.Core.Contracts;
+
+namespace XGate.Core.States
+{
+    public class EnabledDateRangeXGateState : IXGateState
+    {
+        private readonly DateTime _from;
+        private readonly DateTime _to;
+        private readonly bool _isUtc;
+
+        public EnabledDateRangeXGateState(DateTime from, DateTime to, bool isUtc = false)
+        {
+            _from = from;
+            _to = to;
+            _isUtc = isUtc;
+        }
+
+        public bool IsEnabled
+        {
+            get 
+            {  
+                DateTime now = _isUtc ? DateTime.UtcNow : DateTime.Now;
+                return (now >= _from && now <= _to);
+            }
+        }
+    }
+}
