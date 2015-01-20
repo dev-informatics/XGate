6ee3e7af7376d07b5df653b30ffa45f15dcb002d
realityenigma <realityenigma@hotmail.com>
2015-01-20 11:43:27 -0600

    Initial commit of XGate.

---
+++ XGate.EntityFramework/EntityFrameworkValueProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using XGate.Core.Contracts;
+
+namespace XGate.EntityFramework
+{
+    public class EntityFrameworkValueProvider : IXGateStateValueProvider    
+    {
+        private readonly XGateDbContext _context;
+
+        public EntityFrameworkValueProvider(XGateDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public bool IsEnabled(IXGate featureGate)
+        {
+            var featureGateValue =_context.XFeatureGates.FirstOrDefault(feat => feat.Name == featureGate.GetType().Name);
+            if (featureGateValue == null)
+                throw new NullReferenceException("featureGateValue.");
+
+            return featureGateValue.IsEnabled;
+        }
+    }
+}
---
+++ XGate.EntityFramework/XGateDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using XGate.EntityFramework.Models;
+
+namespace XGate.EntityFramework
+{
+    public class XGateDbContext : DbContext
+    {
+        public DbSet<XFeatureGate> XFeatureGates { get; set; }
+    }
+}
